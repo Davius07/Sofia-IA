466e58338078fddb6dbaaeb704a15387d2a03423
Davius07 <126026000+Davius07@users.noreply.github.com>
2023-03-06 20:09:33 -0500

    v1.0

---
+++ run.py
@@ -0,0 +1,53 @@
+import os, time, platform
+
+cargando=0
+
+def clear():
+    time.sleep(0)
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+       os.system("clear") 
+clear()
+
+def iniciando():
+    print(f"""
+            Descargando archivos
+
+
+            Por favor espere:     
+            {cargando}%
+            
+    """)
+
+os.system('pip install speech_recognition')
+cargando = 16
+iniciando()
+os.system('pip install pywhatkit')
+os.system('pip install PIL')
+cargando = 32
+iniciando()
+os.system('pip install wikipedia')
+cargando = 48
+iniciando()
+os.system('pip install pywhatkit')
+cargando = 64
+iniciando()
+#os.system('pip install datetime')
+os.system('pip install keyboard')
+cargando = 80
+iniciando()
+os.system('pip install pygame')
+os.system('pip install tk')
+cargando = 100
+iniciando()
+
+def iniciando():
+    print(f"""
+            Descargando archivos
+
+
+            Por favor espere:     
+            {cargando}%
+            
+    """)
