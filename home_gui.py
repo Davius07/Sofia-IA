ae93105e3ca5bcbb9374dd00bfb4a639d86affd2
Davius07 <juandavidfuentesnovoa78@gmail.com>
2023-03-06 23:58:40 -0500

    v2.3.3

--- home_gui.py
+++ home_gui.py
@@ -18,7 +18,7 @@ import threading as tr
 #variables de interfas gui
 numero_de_version = str("v1.1")
 color_de_fondo = "#ffe0ff"
-color_de_momento = "#ffe0ff"
+color_de_momento = "#89004f"
 
 
 #Ventana
@@ -50,7 +50,7 @@ comandos="""\n\n\n\n\n\n\n
     
     - Escribe
 
-    - Salir
+    - Salir o Termina
 """
 name = "sofia"
 listener = sr.Recognizer()
@@ -60,20 +60,7 @@ engine = pyttsx3.init()
 titulo_photo = ImageTk.PhotoImage(Image.open(r"media\icons\Titulo.png"))
 sofia_photo = ImageTk.PhotoImage(Image.open(r"media\icons\Sofia.png"))
 iniciar_photo = ImageTk.PhotoImage(Image.open(r"media\icons\Iniciar.png"))
-
-#Titulo
-label_title = Label(main_window, image=titulo_photo, bg=color_de_fondo, border=0, relief=FLAT)
-label_title.pack(pady=5)
-
-label_title = Label(main_window, image=sofia_photo, bg=color_de_fondo, border=0, relief=FLAT)
-label_title.pack(pady=5)
-
-label_title = Label(main_window, image=iniciar_photo, bg=color_de_fondo, border=0, relief=FLAT, cursor="hand2")
-label_title.pack(pady=5)
-
-canvas_comandos = Canvas(bg=color_de_momento, height=270, width=200, bd=0, relief=FLAT)
-canvas_comandos.place(x=30, y=10)
-canvas_comandos.create_text(90, 80, text=comandos, fill="black", font="Arial 9")
+leer_photo = ImageTk.PhotoImage(Image.open(r"media\icons\Leer Texto.png"))
 
 
 voices = engine.getProperty("voices")
@@ -117,7 +104,7 @@ def talk(text):
 def listen():
     listener = sr.Recognizer()     
     with sr.Microphone() as source:
-        print("Escuchando...")
+        talk("te estoy escuchando.")
         listener.adjust_for_ambient_noise(source)
         pc = listener.listen(source)
 
@@ -139,6 +126,12 @@ def write(f):
     time.sleep(0)
     talk('Ya puedes ver tu archivo')
     sub.Popen("nota.txt", shell=True)
+    
+#Funciones de la GUI
+
+def read_and_talk():
+    text = text_info.get("1.0", "end")
+    talk(text)
 
 #Funcion Principal
 def run_sofia():
@@ -201,11 +194,32 @@ def run_sofia():
                 file = open("nota.txt", "a")
                 write(file)
 
-        elif 'salir' in rec:
+        elif 'salir' or 'termina' in rec:
             talk('saliendo del sistema')
             break
-        
-        
+
+#GUI 
+#Titulo
+label_title = Label(main_window, image=titulo_photo, bg=color_de_fondo, border=0, relief=FLAT)
+label_title.pack(pady=5)
+
+label_sofia = Label(main_window, image=sofia_photo, bg=color_de_fondo, border=0, relief=FLAT)
+label_sofia.pack(pady=5)
+
+button_iniciar = Button(main_window, image=iniciar_photo, bg=color_de_fondo, border=0, 
+                        relief=FLAT, cursor="hand2", command=run_sofia)
+button_iniciar.pack(pady=5)
+
+canvas_comandos = Canvas(bg=color_de_fondo, height=270, width=200, bd=0, relief=FLAT)
+canvas_comandos.place(x=30, y=10)
+canvas_comandos.create_text(90, 80, text=comandos, fill="black", font="Arial 10")
+
+text_info = Text(main_window, bg="#ffa8d9", relief=FLAT, bd=3, fg='black', borderwidth=15)
+text_info.place(x=590, y=10, height=270, width=200,)
+
+button_leer = Button(main_window, image=leer_photo, bg=color_de_fondo, border=0, 
+                    relief=FLAT, cursor="hand2", command=read_and_talk)
+button_leer.place(x=10, y=300)
         
 
 #Iniciador loop de la ventana
